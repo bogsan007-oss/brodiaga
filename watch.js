98657469d7e92fe4a0a9a0808c8f58e3e5f1183e
bogsan007-oss <bog.san007@gmail.com>
2026-06-19 14:42:08 +0200

    Update watch.js

--- watch.js
+++ watch.js
@@ -45,3 +45,37 @@ async function loadVideo() {
 }
 
 loadVideo();
+async function loadRelatedVideos(currentId) {
+    const apiKey = "AIzaSyDJAfqTtSmIfxH_BMKKuBVMp0qnz7Q5lOg";
+    const playlistId = "UUIRgBQwdKyIY5Sr0JDn4uPQ";
+
+    const url = `https://www.googleapis.com/youtube/v3/playlistItems?key=${apiKey}&playlistId=${playlistId}&part=snippet&maxResults=50`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    const container = document.getElementById("related-videos");
+    container.innerHTML = "";
+
+    data.items
+        .filter(item => item.snippet.resourceId.videoId !== currentId)
+        .slice(0, 8) // показываем 8 похожих
+        .forEach(item => {
+            const videoId = item.snippet.resourceId.videoId;
+            const title = item.snippet.title;
+            const thumb = item.snippet.thumbnails.medium.url;
+
+            const card = document.createElement("div");
+            card.className = "related-card";
+            card.onclick = () => {
+                window.location.href = `watch.html?id=${videoId}`;
+            };
+
+            card.innerHTML = `
+                <img class="related-thumb" src="${thumb}">
+                <div class="related-title-text">${title}</div>
+            `;
+
+            container.appendChild(card);
+        });
+}
+
